8a0c7039d7ace02dcc007c1c3f1e9af902fb90fa
Kevin Sánchez Cano <61790908+ksanchezcano@users.noreply.github.com>
2022-09-02 20:55:46 -0600

    mantenimiento usuarios

--- src/www/sv/cuponera/modelo/UsuariosModel.java
+++ src/www/sv/cuponera/modelo/UsuariosModel.java
@@ -40,7 +40,7 @@ public class UsuariosModel extends Conection {
 		 }
 	}
 	
-	public List<UsuarioBeans> listarEmpreas() throws SQLException{ //listar usuarios
+	public List<UsuarioBeans> listarEmpreas() throws SQLException{ //listar empresas
 		 try {
 			 List<UsuarioBeans> lista = new ArrayList<>();
 			 String sql = "CALL listarEmpresas()";
@@ -60,6 +60,58 @@ public class UsuariosModel extends Conection {
 			 Logger.getLogger(UsuariosModel.class.getName()).log(Level.SEVERE, null, ex);
 			 this.desconectar();
 			 return null;
+		 }
+	}
+	
+	public UsuarioBeans obtenerUsuario(int id) throws SQLException{ //obetener información de un usuario
+		 try {
+		 String sql = "CALL obtenerUsuario(?)";
+		 this.conectar();
+		 cs = conexion.prepareCall(sql);
+		 cs.setInt(1, id);
+		 rs = cs.executeQuery();
+		 if(rs.next()){
+		 UsuarioBeans usuario = new UsuarioBeans();
+		 
+		 usuario.setIdUsuario(rs.getString("idUsuario"));
+		 usuario.setUsername(rs.getString("Username"));
+		 usuario.setNombre(rs.getString("Nombre"));
+		 usuario.setApellido(rs.getString("Apellidos"));
+		 usuario.seteMail(rs.getString("Email"));
+		 usuario.setIdRol(rs.getString("Roles_idRoles"));
+		 usuario.setEmpresa(rs.getString("NombreEmpresa"));
+		 usuario.setDUI(rs.getString("DUI"));
+
+		 
+		 this.desconectar();
+		 return usuario;
+		 }
+		 this.desconectar();
+		 return null;
+		 } catch (SQLException ex) {
+		 Logger.getLogger(UsuariosModel.class.getName()).log(Level.SEVERE, null, ex);
+		 this.desconectar();
+		 return null;
+		 }
+	}
+	
+	public int modificarUsuario(UsuarioBeans usuario) throws SQLException{ //modificar usuario
+		 try {
+		 int filasAfectadas=0;
+		 String sql = "CALL modificarUsuario(?,?,?)";
+		 this.conectar();
+		 cs = conexion.prepareCall(sql);
+		 cs.setString(1, usuario.getIdUsuario());
+		 cs.setString(2, usuario.getNombre());
+		 cs.setString(3, usuario.getApellido());
+
+		 filasAfectadas = cs.executeUpdate();
+		 this.desconectar();
+		 return filasAfectadas;
+		 } catch (SQLException ex) {
+		 Logger.getLogger(UsuariosModel.class.getName()).log(Level.SEVERE, null, ex);
+		 this.desconectar();
+		 return 0;
 		 }
 	}
 	
